8568e23275d739060fbfea23bf434f670f14dfeb
dnyaneshwar1303 <dnyaneshwardpatil2003@gmail.com>
2026-09-24 11:43:58 +0530

    Add product pagination

--- services/productApi.js
+++ services/productApi.js
@@ -1,7 +1,7 @@
 import api from "./api";
 
-export const getProducts=async()=>{
-    const response = await api.get("/products");
+export const getProducts=async(limit = 10, skip = 0)=>{
+    const response = await api.get(`/products?limit=${limit}&skip=${skip}`);
 
     return response.data;
 };
