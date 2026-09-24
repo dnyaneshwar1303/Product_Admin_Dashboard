fcd809b8ea0a45a740f0632ace17b9f38fcda578
dnyaneshwar1303 <dnyaneshwardpatil2003@gmail.com>
2026-09-24 13:52:05 +0530

    add deleteProduct function

--- services/productApi.js
+++ services/productApi.js
@@ -49,4 +49,34 @@ export const getCategories = async () => {
   const response = await api.get("/products/categories");
 
   return response.data;
+  
+};
+
+export const getProductById=async(id, signal)=>{
+  const response = await api.get(`products/${id}`,{
+    signal,
+  });
+
+  return response.data;
+};
+
+export const createProduct = async (productData) => {
+  const response = await api.post("/products/add", productData);
+
+  return response.data;
+};
+
+export const updateProduct = async (id, productData) => {
+  const response = await api.put(
+    `/products/${id}`,
+    productData
+  );
+
+  return response.data;
+};
+
+export const deleteProduct = async (id) => {
+  const response = await api.delete(`/products/${id}`);
+
+  return response.data;
 };
